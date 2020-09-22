5c565923b01fbfb981a80b28b6daf53445b5166c
EmmanuelleB98 <71670678+EmmanuelleB98@users.noreply.github.com>
2020-09-22 11:25:39 +0200

    Update metrics.py

--- gcn/metrics.py
+++ gcn/metrics.py
@@ -1,21 +1,15 @@
+
 import tensorflow as tf
 
 
-def masked_softmax_cross_entropy(preds, labels, mask):
-    """Softmax cross-entropy loss with masking."""
-    loss = tf.nn.l2_loss(preds - labels)
-    mask = tf.cast(mask, dtype=tf.float32)
-    mask /= tf.reduce_mean(mask)
-    loss *= mask
+def loss(preds, labels):
+    loss = (tf.sqrt(tf.reduce_mean((preds - labels)) ** 2) + 5 * 10 ** -4 * tf.nn.l2_loss(preds - labels))
     return tf.reduce_mean(loss)
 
 
-def masked_accuracy(preds, labels, mask):
-    """Accuracy with masking."""
+def accuracy(preds, labels):
+  
     correct_prediction = tf.equal(tf.argmax(preds, 1), tf.argmax(labels, 1))
     accuracy_all = tf.cast(correct_prediction, tf.float32)
-    mask = tf.cast(mask, dtype=tf.float32)
-    mask /= tf.reduce_mean(mask)
-    accuracy_all *= mask
     return tf.reduce_mean(accuracy_all)
 
